b2727a5b63ea4a3766290a9fd2eb6ee73860bab5
James Quiambao <james.quiambao@yahoo.ca>
2016-12-14 14:32:21 -0500

    Setup complete

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 var express = require('express'); 
 var app = express(); 
+var port = process.env.PORT || 80; 
+
+app.get('/', function(req, res){
+	res.end('Hi');
+
+}); 
+
+app.listen(port, function(){
+	console.log("Listening on port: " + port); 
+}); 
