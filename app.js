d749748d782d48a83562173b3242af258872cc93
James <james.quiambao@yahoo.ca>
2017-05-15 17:47:52 -0400

    update project

--- app.js
+++ app.js
@@ -11,13 +11,18 @@ var port = process.env.PORT || 80;
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Template engine 
 app.set('views', './views'); 
 app.set('view engine', 'pug'); 
 
+// Routes 
+
+// Homepage 
 app.get('/', function(req, res){
 	res.render('index', {layout:false, stock:'FB'});
 }); 
 
+// Post request when user enters a stock name 
 app.post('/stock', function(req, res) {
   console.log('You sent the stock"' + req.body.stock + '".');
   res.render('index', {layout: false, stock: req.body.stock});
