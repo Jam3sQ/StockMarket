5dcf2a3aa362ef73848453c1e9337d4a096903c4
James <james.quiambao@yahoo.ca>
2017-05-21 00:48:59 -0400

    ES6

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 //Init express
-var express = require('express'); 
-var app = express(); 
-var path = require('path');
-var bodyParser = require('body-parser'); 
+const express = require('express'); 
+const app = express(); 
+const path = require('path');
+const bodyParser = require('body-parser'); 
 
 //Set Port
-var port = process.env.PORT || 80; 
+const port = process.env.PORT || 80; 
 
 //Body parser
 app.use(bodyParser.urlencoded({ extended: true })); 
@@ -18,16 +18,12 @@ app.set('view engine', 'pug');
 // Routes 
 
 // Homepage 
-app.get('/', function(req, res){
-	res.render('index', {layout:false, stock:'FB'});
-}); 
+app.get('/', (req, res) => res.render('index', {layout:false, stock:'FB'})); 
 
 // Post request when user enters a stock name 
-app.post('/stock', function(req, res) {
+app.post('/stock', (req, res) => {
   console.log('You sent the stock"' + req.body.stock + '".');
   res.render('index', {layout: false, stock: req.body.stock});
 });
 
-app.listen(port, function(){
-	console.log("Listening on port: " + port); 
-}); 
+app.listen(port, () => console.log("Listening on port: " + port)); 
